e1b82e93f0df52cf9f7366d2ad61e14060445b32
Carmine Tramontano <carminetramontano0@gmail.com>
2020-04-23 16:06:52 +0200

    Aggiunta dei commenti al codice face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,17 +1,25 @@
+#Importo la libreria
 import cv2
+#Carico il classificatore
 face_cascade= cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
+#Carico l'immagine
 img = cv2.imread("test.jpg")
+#Il classificare che stiamo utilizzando lavora esclusivamente su immagini in scala di grigi. Per ovviare a
+#questo problema, ci basterà convertire la nostra immagine in questa rappresentazione.
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("volti_grigi.jpg",gray)
+#Sfruttiamo il classificatore salvato in face_cascade, applicandolo all’immagine test, stampandone il contenuto
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 print (faces.shape)
 print (faces)
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+#Disegno un rettangolo attorno al volto con gli elementi presenti in faces
+#for (x, y, w, h) in faces:
+    #cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 #cv2.imshow('img', img)
 #cv2.waitKey()
 #cv2.destroyAllWindows()
-cv2.imwrite("test_rectangle.png",img)
+#cv2.imwrite("test_rectangle.png",img)
+
+#Crop dei volti
 cont = 0
 for (x, y, w, h) in faces:
     img2 = img[ y:y+h , x:x+w, : ]
