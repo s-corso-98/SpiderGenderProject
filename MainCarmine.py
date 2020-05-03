6d17fa5fd308d6bc8e85a8d6bb51f0c852bca62e
Carmine Tramontano <carminetramontano0@gmail.com>
2020-05-03 17:27:39 +0200

    Import del dataset e lettura, aggiornamento delle librerie e primi test.

--- MainCarmine.py
+++ MainCarmine.py
@@ -1,1 +1,35 @@
 #Codice Carmine
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import svm, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+#dataset = pd.read_csv("dataset.csv")
+
+from numpy import genfromtxt
+dataset = genfromtxt('dataset.csv', delimiter=',')
+
+print (dataset.shape)
+
+print (type(dataset))
+
+#Stampa interno dataset
+print (dataset)
+
+#print(dataset.target)
+#Osserviamo i primi cinque array
+#print(dataset.head())
