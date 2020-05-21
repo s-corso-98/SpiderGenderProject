63dac43ff7a8b420a5b8c6f97fb7b390d046ecbe
s-corso-98 <silvio.corso.98@gmail.com>
2020-05-21 09:24:03 +0200

    Reorganized "Main" and "MainJupyter" files.

--- Main.py
+++ Main.py
@@ -8,56 +8,6 @@ import time
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 
-start_time = time.time()
-
-#Caricamento dei due dataset
-dataframe = pd.read_csv("DatasetCelebA/dataset4c4s.csv",header=None)
-feature = pd.read_csv("DatasetCelebA/list_attr_celeba.csv")
-
-
-#Prendo la colonna delle features riguardante il sesso
-feat = feature.iloc[0:202599,21]
-df_X = pd.DataFrame(feat)
-
-#Rinonimo la colonna da Male a Gender
-rename = df_X.rename(columns={"Male" : "Gender"}) #-1 donna e 1 maschio
-
-#Concateno i due dataframe per crearne uno
-dfconc = pd.concat([dataframe, rename], axis=1, sort=False)
-
-#Ottengo feature variables
-feature_cols = list(dfconc.columns.values)
-X = feature_cols[1:len(feature_cols)-1]
-X = dfconc[X]
-
-#Ottengo target variables
-y = dfconc.Gender
-
-#Divido il dataframe in train e test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
-
-def MyDecisionTree():
-    #The decision tree classifier
-    clf = tree.DecisionTreeClassifier(criterion = "gini", max_depth=13)
-
-    #Alleno il decision tree
-    clf_train = clf.fit(X_train, y_train)
-
-    #Predico la risposta per il dataset
-    y_pred = clf.predict(X_test)
-
-    #Model Accuracy, valuto il modello
-    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-    print ("\nTempo trascorso: {:.2f}m\n".format((time.time()-start_time)/60))
-
-    #Creo un decision tree in formato DOT utilizzando GraphViz
-    dot_data = tree.export_graphviz(clf_train, out_file=None, feature_names=X_train.columns.values,
-                                    class_names=['Female', 'Male'], rounded=True, filled=True) #Gini decides which attribute/feature should be placed at the root node, which features will act as internal nodes or leaf nodes
-    #Creo il decision tree in formato Graph partendo dal formato DOT
-    graph = pydotplus.graph_from_dot_data(dot_data)
-    #Salvo in png il decision tree creato
-    test2 = graph.write_png("OutputDT/GraphDecisionTree.png")
-
 
 def MySupportVectorMachine():
    #Creazione del classificatore
@@ -98,6 +48,35 @@ def MySupportVectorMachine():
    print(accuracy_score(y_test, predicted))
 
 
+
+
+
+
+
+def MyDecisionTree():
+    #The decision tree classifier
+    clf = tree.DecisionTreeClassifier(criterion = "gini", max_depth=13)
+
+    #Alleno il decision tree
+    clf_train = clf.fit(X_train, y_train)
+
+    #Predico la risposta per il dataset
+    y_pred = clf.predict(X_test)
+
+    #Model Accuracy, valuto il modello
+    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+    print ("\nTempo trascorso: {:.2f}m\n".format((time.time()-start_time)/60))
+
+    #Creo un decision tree in formato DOT utilizzando GraphViz
+    dot_data = tree.export_graphviz(clf_train, out_file=None, feature_names=X_train.columns.values,
+                                    class_names=['Female', 'Male'], rounded=True, filled=True) #Gini decides which attribute/feature should be placed at the root node, which features will act as internal nodes or leaf nodes
+    #Creo il decision tree in formato Graph partendo dal formato DOT
+    graph = pydotplus.graph_from_dot_data(dot_data)
+    #Salvo in png il decision tree creato
+    test2 = graph.write_png("OutputDT/GraphDecisionTree.png")
+
+
+
 def MyNearestNeighbors():
     #NearestNeighbors classifier
     classifier = KNeighborsClassifier(n_neighbors=9)
@@ -128,6 +107,35 @@ def MyNearestNeighbors():
     plt.yticks(positions,labels)
     plt.savefig('OutputKNN/ConfusionMatrix4c4s_n9.png', bbox_inches='tight')
     plt.show()
+
+
+start_time = time.time()
+
+#Caricamento dei due dataset
+dataframe = pd.read_csv("DatasetCelebA/dataset4c4s.csv",header=None)
+feature = pd.read_csv("DatasetCelebA/list_attr_celeba.csv")
+
+
+#Prendo la colonna delle features riguardante il sesso
+feat = feature.iloc[0:202599,21]
+df_X = pd.DataFrame(feat)
+
+#Rinonimo la colonna da Male a Gender
+rename = df_X.rename(columns={"Male" : "Gender"}) #-1 donna e 1 maschio
+
+#Concateno i due dataframe per crearne uno
+dfconc = pd.concat([dataframe, rename], axis=1, sort=False)
+
+#Ottengo feature variables
+feature_cols = list(dfconc.columns.values)
+X = feature_cols[1:len(feature_cols)-1]
+X = dfconc[X]
+
+#Ottengo target variables
+y = dfconc.Gender
+
+#Divido il dataframe in train e test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
 
 #Esecuzione classificatori
 MyDecisionTree()
