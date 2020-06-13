ee4e7da4d7a14fa75e815572d03179473b8f0981
s-corso-98 <silvio.corso.98@gmail.com>
2020-06-13 13:14:24 +0200

    Created the balanced dataset based on the new method (without spider web). Executed the tests and saved the results.

--- Main.py
+++ Main.py
@@ -88,11 +88,6 @@ def MyNearestNeighbors():
     #Predico la risposta per il dataset
     y_pred = classifier.predict(X_test)
 
-    #Model Accuracy, valuto il modello
-    print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-    print ("\nTempo trascorso: {:.2f}m\n".format((time.time()-start_time)/60))
-
-
     labels = ("Female","Male")
     positions = (0,1)
 
@@ -103,10 +98,14 @@ def MyNearestNeighbors():
     disp.figure_.suptitle("Confusion Matrix")
     print("Confusion matrix:\n%s" % disp.confusion_matrix)
 
+    # Model Accuracy, valuto il modello
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    print("\nTempo trascorso: {:.2f}m\n".format((time.time() - start_time) / 60))
+
     #Stampa a video
     plt.xticks(positions,labels)
     plt.yticks(positions,labels)
-    plt.savefig('OutputKNN/ConfusionMatrix4c4s_n9.png', bbox_inches='tight')
+    plt.savefig('OutputKNN/ConfusionMatrixNoWeb_n9.png', bbox_inches='tight')
     plt.show()
 
 
@@ -115,23 +114,23 @@ def MyNearestNeighbors():
 #Funzione creazione dataset bilanciato
 def CreateBalanced4c4s(dfconc):
     #Creo un csv corrispondente al dataset4c4s ma con l'aggiunta della colonna "Gender"
-    dfconc.to_csv("DatasetCelebA/Dataset4c4sBalanced.csv", header = False, index = False)
+    dfconc.to_csv("DatasetCelebA/DatasetBalancedNoWeb.csv", header = False, index = False)
     #Leggo il csv appena creato per andare ad eseguire le operazioni di manipolazione
-    DFbalanced = pd.read_csv("DatasetCelebA/Dataset4c4sBalanced.csv",header = None)
+    DFbalanced = pd.read_csv("DatasetCelebA/DatasetBalancedNoWeb.csv",header = None)
 
     #Salvo in un dataframe tutte le righe con gender pari a 1(uomo)
-    dfBalanceM = DFbalanced.loc[DFbalanced[64] == 1]
+    dfBalanceM = DFbalanced.loc[DFbalanced[128] == 1]
     #Salvo in un dataframe tutte le righe con gender pari a -1(donna)
-    dfBalanceF = DFbalanced.loc[DFbalanced[64] == -1]
+    dfBalanceF = DFbalanced.loc[DFbalanced[128] == -1]
 
     #Droppo le righe in eccesso del dataframe femminile (rispetto al dataframe maschile)
-    dfBalanceF = dfBalanceF.iloc[0:84434]
+    dfBalanceF = dfBalanceF.iloc[0:432]
 
     #Unisco i due dataframe aventi lo stesso numero di elementi
     DFbalanced = pd.concat([dfBalanceM,dfBalanceF], axis = 0)
 
     #Creo il csv corrispondente
-    DFbalanced.to_csv("DatasetCelebA/Dataset4c4sBalanced.csv", header = False, index = False)
+    DFbalanced.to_csv("DatasetCelebA/DatasetBalancedNoWeb.csv", header = False, index = False)
 
 
 
@@ -139,10 +138,10 @@ def CreateBalanced4c4s(dfconc):
 #Funzione per suddividere il dataset bilanciato in train e test set
 def ExecOnBalanced():
     #Leggo dataset bilanciato
-    dataframe = pd.read_csv("DatasetCelebA/dataset4c4sBalanced.csv", header=None)
+    dataframe = pd.read_csv("DatasetCelebA/DatasetBalancedNoWeb.csv", header=None)
 
     #Rinomino la colonna 64 in Gender.
-    dataframe = dataframe.rename(columns={dataframe.columns[64]: "Gender"})  # -1 donna e 1 maschio
+    dataframe = dataframe.rename(columns={dataframe.columns[128]: "Gender"})  # -1 donna e 1 maschio
 
     #Ottengo feature variables
     feature_cols = list(dataframe.columns.values)
@@ -164,11 +163,11 @@ def ExecOnBalanced():
 #Funzione lettura CSV su cui eseguire i test
 def ReadCSV():
     # Caricamento dei due dataset
-    dataframe = pd.read_csv("DatasetCelebA/dataset4c4s1000.csv", header=None)
+    dataframe = pd.read_csv("DatasetCelebA/datasetnoweb.csv", header=None)
     feature = pd.read_csv("DatasetCelebA/list_attr_celeba.csv")
 
     # Prendo la colonna delle features riguardante il sesso.
-    feat = feature.iloc[0:1000, 21]
+    feat = feature.iloc[0:1003, 21]
     df_X = pd.DataFrame(feat)
 
     # Rinonimo la colonna da Male a Gender.
@@ -209,6 +208,6 @@ else:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
 
 #Esecuzione classificatori
-MyDecisionTree()
+#MyDecisionTree()
 #MyNearestNeighbors()
-#MySupportVectorMachine()
+MySupportVectorMachine()
